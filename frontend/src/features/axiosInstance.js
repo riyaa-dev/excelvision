83c49c9b5ccf9efec28c22d37b7e8dd211f99cb1
sainiriyaa44444@gmail.com <user.email like : "riyasainii566@gmail.com>
2025-07-24 09:31:08 +0530

    Fix token retrieval in axios interceptor

--- frontend/src/features/axiosInstance.js
+++ frontend/src/features/axiosInstance.js
@@ -8,11 +8,17 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem().auth.token;
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
-});
+},
+
+(error) => Promise.reject(error)
+
+);
+
+
 
 export default axiosInstance;
