582db146456720c5545b5dc8d9cc61ffe75c30d8
sainiriyaa44444@gmail.com <user.email like : "riyasainii566@gmail.com>
2025-07-30 13:59:16 +0530

    Add chart saving functionality to chartRoutes for persistent chart storage

---
+++ backend/routes/chartRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const Chart = require("../models/ChartModel");
+const authMiddleware = require("../middleware/authMiddleware");
+
+
+// save chart
+router.post("/save", authMiddleware,async (req , res)=>{
+    try{
+        const {xAxis , yAxis , chartType , data} = req.body;
+        const newChart = new Chart({
+            user : req.user.id;
+            xAxis,
+            yAxis,
+            chartType,
+            data,
+        });
+        await newChart.save();
+        res.status(201).json({message : "Chart saved succesfully !"});  
+
+    }catch(err){
+        res.status(500).json({message : "Failed to save chart"});
+        
+    }
+})
+
+module.exports=router;
