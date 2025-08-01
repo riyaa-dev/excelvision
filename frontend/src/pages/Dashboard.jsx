152bee85695eb1270161f43a864b1a59289c550c
sainiriyaa44444@gmail.com <user.email like : "riyasainii566@gmail.com>
2025-08-01 20:13:36 +0530

    Comment out savedCharts state declaration in Dashboard component

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import axiosInstance from '../features/axiosInstance';
 import ChartRenderer from '../components/ChartRenderer';
-const [savedCharts, setSavedCharts] = useState([]);
+// const [savedCharts, setSavedCharts] = useState([]);
 
 
 const Dashboard = () => {
